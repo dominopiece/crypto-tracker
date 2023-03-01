46a301bf34666ae29b3a0e07e6e0f0a38603e5d3
dominopiece <dominopiece@gmail.com>
2023-03-02 02:29:26 +0900

    css, router(Chart, Price)

--- crypto-tracker/src/routes/Coin.tsx
+++ crypto-tracker/src/routes/Coin.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -25,6 +25,31 @@ const Loader = styled.span`
   display: block;
   margin-top: 15px;
   font-size: 20px;
+`;
+
+const OverView = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 7px;
+  }
+`;
+
+const Decription = styled.p`
+  margin: 20px 0px;
 `;
 
 interface LocationState {
@@ -123,22 +148,62 @@ function Coin() {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
-      console.log(infoData);
+      // console.log(infoData);
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-      console.log(priceData);
+      // console.log(priceData);
       setInfo(infoData);
       setPriceInfo(priceData);
-      // setLoading(false)
+      setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
   return (
     <Container>
       <Header>
-        <Title>{state || "secret mode or direct address"}</Title>
+        <Title>
+          {/* 시크릿 모드에서 접속 시 아래 문구  */}
+          {/* {state || "secret mode or direct address"} */}
+          {state ? state : loading ? "Loading.." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading..</Loader> : <span>{info?.id}</span>}<span>{priceInfo?.quotes.USD.price}</span>
+      {loading ? <Loader>Loading..</Loader> : <span>{info?.id}</span>}
+      {loading ? (
+        <Loader>Loading..</Loader>
+      ) : (
+        <>
+          <OverView>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </OverView>
+          <Decription>{info?.description}</Decription>
+          <OverView>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply: </span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Price USD: </span>
+              <span>${priceInfo?.quotes.USD.price}</span>
+            </OverviewItem>
+          </OverView>
+          <Outlet />
+        </>
+      )}
     </Container>
   );
 }
